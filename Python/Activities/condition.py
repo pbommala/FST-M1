91e9e5bb6a33bd25ad35a6c2e9be575aac3b2a46
pbommala <110019406+pbommala@users.noreply.github.com>
2022-10-21 12:38:39 +0530

    Add files via upload

---
+++ Python/Activities/condition.py
@@ -0,0 +1,19 @@
+a = 5
+b = 7
+c = 9
+
+if a > b:
+  if a > c:
+    print("a is the greatest")
+  else:
+    print("c is the greatest")
+elif b > a:
+  if b > c:
+    print("b is the greatest")
+  else:
+    print("c is the greatest")
+    
+    should_i_exit = true
+
+if should_i_exit == true:
+  raise SystemExit
